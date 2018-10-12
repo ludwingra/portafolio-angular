c7aaabaac3be0329b1845e7a82873955af888e92
Ludwing.ra <ludwing.ra@gmail.com>
2018-10-12 00:55:15 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any = [];
 
   constructor( private http: HttpClient ) {
-    // tslint:disable-next-line:comment-format
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
+
+   private cargarInfo() {
+     // tslint:disable-next-line:comment-format
     //Leer el archivo JSON usando peticiones HTTP
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+   }
 
+   private cargarEquipo() {
+      this.http.get('https://angular-template-9f884.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        // console.log(resp);
+      });
    }
 }
